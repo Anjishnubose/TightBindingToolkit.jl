d55db21f6f707de406e423255faef549fa4fa1ee
Anjishnu <anjishnubose98@gmail.com>
2023-05-21 16:05:38 +0530

    Julia documenter make file

---
+++ docs/make.jl
@@ -0,0 +1,22 @@
+using Documenter
+using TightBindingToolkit
+
+makedocs(
+    build       =   "build" ,
+    sitename    =   "TightBindingToolkit.jl"    ,
+    modules     =   Module[TightBindingToolkit]   ,
+    pages = [
+        "Introduction"              =>  "index.md",
+        "Unit Cell"                 =>  "UnitCell.md",
+        "Brillouin Zone"            =>  "BZ.md",
+        "Hamiltonian"               =>  "Hamiltonian.md",
+        "Tight Binding Model"       =>  "Model.md",
+        "Chern Numbers"             =>  "Chern.md",
+        "Magnetic susceptibility"   =>  "susceptibility.md"  
+    ]
+)
+
+deploydocs(
+    repo = "github.com/Anjishnubose/TightBindingToolkit.jl.git",
+    devbranch = "main"
+)
