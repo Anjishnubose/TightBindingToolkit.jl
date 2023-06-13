442d1ff02db823236b3abcb595be47a380753dfe
Anjishnu <anjishnubose98@gmail.com>
2023-06-13 15:30:33 +0530

    Updated package module

--- src/TightBindingToolkit.jl
+++ src/TightBindingToolkit.jl
@@ -2,24 +2,35 @@ module TightBindingToolkit
 
 # Write your package code here.
 include("SpinMats.jl")
+using .SpinMatrices
 export SpinMats
 
 include("UnitCell.jl")
-export Bond , isSameBond , UnitCell , getDistance , addBasisSite! , addAnisotropicBond! , addIsotropicBonds! , ModifyBonds! , ScaleBonds! , RemoveBonds! , ModifyFields!
+using .UCell
+export Bond , isSameBond , UnitCell , getDistance , addBasisSite! , addAnisotropicBond! , addIsotropicBonds! , ModifyBonds! , ScaleBonds! , RemoveBonds! , ModifyFields!, ModifyIsotropicFields!
+
+include("Params.jl")
+using .Parameters
+export Param, addAnisotropicBond!, addIsotropicBonds!, CreateUnitCell!, ModifyUnitCell!, GetParams
 
 include("BZ.jl")
-export getRLVs , BZ , fillBZ! , ReduceQ , GetQIndex , GetIndexPath , getBZPath , CombinedBZPath
+using .BZone
+export getRLVs , BZ , fillBZ! , Monkhorst, ReduceQ , GetQIndex , GetIndexPath , getBZPath , CombinedBZPath
 
 include("Hamiltonian.jl")
-export Hamiltonian , FillHamiltonian , DiagonalizeHamiltonian! , DOS
+using .Hams
+export Hamiltonian , FillHamiltonian , FullHamiltonian , DiagonalizeHamiltonian! , DOS
 
 include("Model.jl")
+using .TBModel
 export Model , dist , distDer , findFilling , getMu! , getFilling! , getCount , getGk! , SolveModel!
 
 include("Chern.jl")
+using .Chern
 export FindLinks , FieldStrength , ChernNumber
 
 include("Susceptibility.jl")
+using .suscep
 export susceptibility , FindChi , FillChis!
 
 end
