e5281e0b4967e5b1c73051a93b6e63af27ae1d41
Anjishnu <anjishnubose98@gmail.com>
2023-06-15 11:47:02 +0530

    Updated sample codes

--- Sample/Haldane_Model.jl
+++ Sample/Haldane_Model.jl
@@ -1,4 +1,5 @@
-using TightBindingToolkit, Plots
+using Plots
+using TightBindingToolkit
 
 """
 This script sets up the famous Haldane Model on a honeycomb lattice, and finds it chern numbers.
@@ -9,7 +10,7 @@ The lattice primitive vector for a honeycomb lattice : a1 and a2.
 """
 a1 = [ 1/2 , sqrt(3)/2 ]
 a2 = [-1/2 , sqrt(3)/2 ]
-UC = UnitCell( [a1 , a2] , 1) ##### localDim = 1 cause its a spinless Hamiltonian
+UC = UnitCell( [a1 , a2] , 2) ##### localDim = 2 cause we are Constructing a spinful version of the Haldane Model.
 
 """
 The two sub-lattices of a honeycomb lattice.
@@ -19,49 +20,53 @@ b2 = [ 0.0 , 1/sqrt(3) ]
 addBasisSite!( UC , b1 )
 addBasisSite!( UC , b2 )
 
+SpinVec     =   SpinMats(1//2)
+
 """
 Adding structure to the lattice now, through the bond objects.
 """
 ################## On-site ###################
-const M   =   1.0
-addAnisotropicBond!( UC , 1 , 1 , [ 0 , 0 ]  ,  M , 0.0 , "M")
-addAnisotropicBond!( UC , 2 , 2 , [ 0 , 0 ]  , -M , 0.0 , "M")
+const M     =   1.0
+MParam      =   Param(M)   
+addAnisotropicBond!( MParam, UC , 1 , 1 , [ 0 , 0 ]  ,  SpinVec[4], 0.0 , "M")
+addAnisotropicBond!( MParam, UC , 2 , 2 , [ 0 , 0 ]  , -SpinVec[4], 0.0 , "M")
 ################# Nearest Neighbour hopping #################
-const t1  =   -1.0
+const t1    =   -1.0
+t1Param     =   Param(t1)   
 const NNdistance  =   1/sqrt(3)
-addIsotropicBonds!(UC , NNdistance, t1 , "t1")
+addIsotropicBonds!(t1Param, UC , NNdistance, SpinVec[4] , "t1")
 ################# 2nd Nearest Neighbour #################
-const t2  =   (-1.0) * im
+const t2    =   (-1.0) 
+t2Param     =    Param(t2)  
 const secondNNdistance = 1.0 
-addAnisotropicBond!( UC , 1 , 1 , [ 1 , 0 ]  , t2 , secondNNdistance , "t2")
-addAnisotropicBond!( UC , 1 , 1 , [ 0 , -1 ] , t2 , secondNNdistance , "t2")
-addAnisotropicBond!( UC , 1 , 1 , [ -1 , 1 ] , t2 , secondNNdistance , "t2")
-addAnisotropicBond!( UC , 2 , 2 , [ 0 , 1 ]  , t2 , secondNNdistance , "t2")
-addAnisotropicBond!( UC , 2 , 2 , [ -1 , 0 ] , t2 , secondNNdistance , "t2")
-addAnisotropicBond!( UC , 2 , 2 , [ 1 , -1 ] , t2 , secondNNdistance , "t2")
+addAnisotropicBond!(t2Param, UC , 1 , 1 , [ 1 , 0 ]  , im * SpinVec[4], secondNNdistance , "t2")
+addAnisotropicBond!(t2Param, UC , 1 , 1 , [ 0 , -1 ] , im * SpinVec[4], secondNNdistance , "t2")
+addAnisotropicBond!(t2Param, UC , 1 , 1 , [ -1 , 1 ] , im * SpinVec[4], secondNNdistance , "t2")
+addAnisotropicBond!(t2Param, UC , 2 , 2 , [ 0 , 1 ]  , im * SpinVec[4], secondNNdistance , "t2")
+addAnisotropicBond!(t2Param, UC , 2 , 2 , [ -1 , 0 ] , im * SpinVec[4], secondNNdistance , "t2")
+addAnisotropicBond!(t2Param, UC , 2 , 2 , [ 1 , -1 ] , im * SpinVec[4], secondNNdistance , "t2")
 
+CreateUnitCell!(UC, [MParam, t1Param, t2Param])
 """
 Making the Brillouin Zone with kSize * kSize discrete points.
 """
 const kSize   =   6 * 20 + 3   ##### a Monkhorst grid of size N = 6n+3 covers all the High-symemtry points.
-bz            =   BZ(kSize)
+bz            =   BZ(kSize)    ##### Make the BZ explicitly in 2d
 fillBZ!(bz, UC)
 
 """
 Spanning over different t2/M values
 """
-t2s     =   range(-1.0, 1.0, 50) * M  
+t2s     =   range(-1.0, 1.0, 11) * M  
 gaps    =   Float64[]
 mus     =   Float64[]
 Cherns  =   Float64[]
-for (i, t2Val) in enumerate(t2s)
 
-    if i>1
-        ScaleBonds!(UC, "t2", t2Val / t2s[i-1])
-    else
-        ScaleBonds!(UC, "t2", t2Val / (-1))
-    end
-    ##### Since t2 bonds are anisotropic, the simpler function ModifyBonds! wont work.
+for t2Val in t2s
+
+    push!(t2Param.value, t2Val)
+    ModifyUnitCell!(UC, [t2Param])
+
     """
     Constructing the Hamiltonian in k-space, and diagonalizing it.
     """
@@ -77,18 +82,13 @@ for (i, t2Val) in enumerate(t2s)
     push!(gaps, HaldaneModel.gap)
 
     """
-    Then the chern numbers of the first band in the Haldane Model becomes : 
+    Then the chern numbers of the lowest two bands (1 and 2) in the Haldane Model becomes : 
     """
-    chern   =   ChernNumber(H, [1])
+    chern   =   ChernNumber(H, [1, 2])
     push!(Cherns, chern)
 end
 
-# println("The chemical potential at half filling, μ = ", HaldaneModel.mu)
-# println("The energy gap at half filling, gap = ", HaldaneModel.gap)
-
-# println("The chern number of the lowest band is ",chern)
-gapPlot     = plot(t2s, gaps, labels="gap", lw=3)
-chernPlot   = plot(t2s, Cherns, labels="chern", lw=3)
+chernPlot   = plot(t2s, Cherns, labels="chern", lw=3, markershape = :circle )
 
 
 
--- Sample/Triangle_123NN.jl
+++ Sample/Triangle_123NN.jl
@@ -1,5 +1,6 @@
+using Plots
+using TightBindingToolkit
 
-using Plots, TightBindingToolkit
 """
 This script sets up a simple triangular lattice with first, second, and third neighbour hoppings.
 It then calculates the magnetic susceptibility at zero energy, along a path in the brillouin zone through the Γ, K, and M points.
@@ -23,17 +24,22 @@ Adding structure to the lattice now, through the bond objects.
 """
 SpinVec     =   SpinMats(1//2) ##### Working with spin-1/2
 ################ Nearest Neighbour #################
-const t1  =   -1.0
+const t1    =   -1.0
+t1Param     =   Param(t1)   
 const NNdistance  =   1.0
-addIsotropicBonds!(UC , NNdistance, t1 * SpinVec[4] , "t1")
+addIsotropicBonds!(t1Param, UC , NNdistance, SpinVec[4] , "t1")
 ############### 2nd Nearest Neighbour #################
-const t2  =   0.25
+const t2    =   0.25
+t2Param     =   Param(t2)  
 const secondNNdistance  =   sqrt(3)
-addIsotropicBonds!(UC , secondNNdistance, t2 * SpinVec[4] , "t2" ; checkOffsetRange = 2)
+addIsotropicBonds!(t2Param, UC , secondNNdistance, SpinVec[4] , "t2" ; checkOffsetRange = 2)
 ############### 3rd Nearest Neighbour #################
-const t3  =   -0.25
+const t3    =   -0.25
+t3Param     =   Param(t3)   
 const thirdNNdistance  =   2.0
-addIsotropicBonds!(UC , thirdNNdistance, t3 * SpinVec[4] , "t3", checkOffsetRange = 3)
+addIsotropicBonds!(t3Param, UC , thirdNNdistance, SpinVec[4] , "t3" ; checkOffsetRange = 3)
+
+CreateUnitCell!(UC, [t1Param, t2Param, t3Param])
 
 """
 Making the Brillouin Zone with kSize * kSize discrete points.
@@ -46,7 +52,8 @@ fillBZ!(bz, UC)
 """
 A path in the discretized BZ which goes through Γ -> K -> M -> Γ.
 """
-path = CombinedBZPath(bz, [bz.HighSymPoints["Gamma"], bz.HighSymPoints["K1"], bz.HighSymPoints["M1"]] ; nearest=true)
+path = CombinedBZPath(bz, [bz.HighSymPoints["Gamma"], bz.HighSymPoints["K1"], bz.HighSymPoints["M2"]] ; nearest=true)
+##### The keyword nearest=true since the Monkhorst grid can never actually have the M points in it, so you have to find the point on the grid closest to them.
 
 """
 Some model parameters : temperature and chemical potential
@@ -61,13 +68,16 @@ Filling the model at this chemical potential with spin-1/2 fermions.
 """
 TriangleModel   =   Model(UC, bz, H; T=T, mu=mu)
 SolveModel!(TriangleModel)
-println("The filling at μ = 0 is", TriangleModel.filling)
+println("The filling at μ = 0 is ", TriangleModel.filling)
 
 Omegas  =   [0.0]
-const spread  = 1e-3   
+const spread  = 1e-3  
+dos = DOS(Omegas, H)    ##### the single particle density of states
+
 suscep  =   susceptibility(TriangleModel, path, Omegas ; eta=spread)
 FillChis!(suscep)
-plot(real.(suscep.chis["zz"][1, :]), labels="χ_zz(Ω=0, Q)", lw=3.0)
+##### Plotting the real part of the magnetic susceptibility at 0 energy, and momentum points along the path.
+plot(real.(suscep.chis["zz"][1, :]), labels="χ_zz(Ω=0, Q)", lw=2.0, markershape=:circle, markercolor="red")
 
 
 
