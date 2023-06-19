9f278a145538d81a92d82893bec50ba7a78281b1
Anjishnu <anjishnubose98@gmail.com>
2023-06-19 13:45:27 +0530

    Updated package module

--- src/TightBindingToolkit.jl
+++ src/TightBindingToolkit.jl
@@ -7,7 +7,7 @@ export SpinMats
 
 include("UnitCell.jl")
 using .UCell
-export Bond , isSameBond , UnitCell , getDistance , addBasisSite! , addAnisotropicBond! , addIsotropicBonds! , ModifyBonds! , ScaleBonds! , RemoveBonds! , ModifyFields!, ModifyIsotropicFields!
+export Bond , isSameBond , UnitCell , isSameUnitCell , getDistance , addBasisSite! , addAnisotropicBond! , addIsotropicBonds! , ModifyBonds! , ScaleBonds! , RemoveBonds! , ModifyFields!, ModifyIsotropicFields!
 
 include("Params.jl")
 using .Parameters
@@ -15,15 +15,19 @@ export Param, addAnisotropicBond!, addIsotropicBonds!, CreateUnitCell!, ModifyUn
 
 include("BZ.jl")
 using .BZone
-export getRLVs , BZ , fillBZ! , Monkhorst, ReduceQ , GetQIndex , GetIndexPath , getBZPath , CombinedBZPath
+export getRLVs , BZ , fillBZ! , Monkhorst, ReduceQ , GetQIndex , GetIndexPath , CombinedIndexPath , getBZPath , CombinedBZPath
 
 include("Hamiltonian.jl")
 using .Hams
-export Hamiltonian , FillHamiltonian , FullHamiltonian , DiagonalizeHamiltonian! , DOS
+export Hamiltonian , FillHoppingHamiltonian, FillPairingHamiltonian, FillHamiltonian , DiagonalizeHamiltonian! , DOS, ModifyHamiltonianField!
 
 include("Model.jl")
 using .TBModel
-export Model , dist , distDer , findFilling , getMu! , getFilling! , getCount , getGk! , SolveModel!
+export Model , dist , distDer , findFilling , getMu! , getFilling! , getCount , getGk! , SolveModel! , BinarySearch
+
+include("BdGModel.jl")
+using .BdG
+export BdGModel, findFilling, getMu!, getFilling!, getGk!, SolveModel!
 
 include("Chern.jl")
 using .Chern
@@ -32,5 +36,9 @@ export FindLinks , FieldStrength , ChernNumber
 include("Susceptibility.jl")
 using .suscep
 export susceptibility , FindChi , FillChis!
+
+include("Plot.jl")
+using .PlotTB
+export plot_UnitCell! , plot_band_contour!, plot_band_structure!, plot_FS!
 
 end
