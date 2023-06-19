6783b1f52f913079420c111319db877f41387de8
Anjishnu <anjishnubose98@gmail.com>
2023-06-19 13:44:01 +0530

    Made everything dimension agnostic arrays

--- src/BZ.jl
+++ src/BZ.jl
@@ -1,5 +1,5 @@
 module BZone
-    export getRLVs , BZ , Monkhorst , fillBZ! , ReduceQ , GetQIndex , GetIndexPath , getBZPath , CombinedBZPath
+    export getRLVs , BZ , Monkhorst , fillBZ! , ReduceQ , GetQIndex , GetIndexPath , CombinedIndexPath , getBZPath , CombinedBZPath
  
     using LinearAlgebra
     using ..TightBindingToolkit.UCell: UnitCell
@@ -42,26 +42,34 @@ module BZone
 
     # Attributes
     - `basis           :: Vector{ Vector{ Float64 } }`: reciprocal lattice vectors of the Brillouin Zone.
-    - `gridSize        :: Int64`: The number of points along each dimension of the grid.
-    - `kInds           :: Vector{Matrix{Float64}}`: the [`Monkhorst`](@ref) grid corresponding to k along bs.
-    - `ks   	       :: Matrix{Vector{Float64}}`: The grid of momentum points [kx, ky].
+    - `gridSize        :: Vector{Int64}`: The number of points along each dimension of the grid.
+    - `kInds           :: Vector{Array{Float64}}`: the [`Monkhorst`](@ref) grid corresponding to k along bs.
+    - `ks   	       :: Array{Vector{Float64}}`: The grid of momentum vectors k.
     - `HighSymPoints   :: Dict`: A dictionary containing the HIgh-Symmetry points Γ, K(2), and M(3).
     - `shift           :: Vector{Int64}` : how shifted the grid is from its centre point at the Γ point, in units of `1/gridSize`.
 
     Initialize this structure using 
     ```julia
-    BZ(gridSize::Int64)
+    BZ(gridSize::Int64) --> defaults to dims = 2
+    BZ(gridSize::Int64, dims) --> Uniform grid in dimension = dims
+    BZ(gridSize::Vector{Int64})
     ```
     """
     mutable struct BZ
         basis	        ::  Vector{Vector{Float64}}
-        gridSize        ::  Int64
-        kInds           ::  Vector{Matrix{Float64}}
-        ks   	        ::	Matrix{Vector{Float64}}
+        gridSize        ::  Vector{Int64}
+        kInds           ::  Vector{Array{Float64}}
+        ks   	        ::	Array{Vector{Float64}}
         HighSymPoints   ::  Dict
         shift           ::  Vector{Int64}
-
-        BZ(gridSize::Int64)    =   new{}(Vector{Float64}[], gridSize, Matrix{Float64}[], Array{Vector{Float64}}(undef, 0, 0), Dict(), Int64[])
+    
+        BZ(gridSize::Vector{Int64})         =   new{}(Vector{Float64}[],  gridSize, Array{Float64, length(gridSize)}[], Array{Vector{Float64}}(undef, zeros(Int64, length(gridSize))...), Dict(), Int64[])
+        BZ(gridSize::Int64, dims::Int64) 	=	new{}(Vector{Float64}[], repeat([gridSize], dims), Array{Float64, dims}[], Array{Vector{Float64}}(undef, zeros(Int64, dims)...), Dict(), Int64[])
+        function BZ(gridSize::Int64)
+            println("WARNING : positional argument `dims' not passed in BZ. Resorting to its default value of 2.")
+            dims 	=	2
+            return   new{}(Vector{Float64}[], repeat([gridSize], dims), Array{Float64, dims}[], Array{Vector{Float64}}(undef, zeros(Int64, dims)...), Dict(), Int64[])
+        end
     end
 
 
@@ -87,12 +95,12 @@ module BZone
         end
     end
 
-
+    ##### Angle b/w two vectors
     function VecAngle(a::Vector{Float64}, b::Vector{Float64})
         return acos(clamp(a⋅b/(norm(a)*norm(b)), -1, 1))
     end
 
-
+    ##### returns a meshgrid of (i_1, i_2, ..., i_n) such that i_j ∈ [1, 2, ..., grid[j]] ∀ j in [1, 2, ..., n = length(grid)]
     function meshgrid(grid::Vector{Int64})
         inds    =   []
         for dim in 1:length(grid)
@@ -103,27 +111,59 @@ module BZone
         return inds
     end
 
+    ##### Adds high symmetry points to the bz dictionary
+    function AddHighSymPoints!(bz::BZ)
+        dims    =   length(bz.basis)
+        
+        ##### Gamma point at the origin.
+        bz.HighSymPoints["G"]   =   zeros(Float64, dims)
+        
+        if dims == 1    ##### 1d BZ
+            bz.HighSymPoints["M1"]      =   @. 0.5 * bz.basis[1]    ##### M point at the midpoint of the BZ (pi)
+            bz.HighSymPoints["K1"]      =   @. (1/3) * bz.basis[1]  ##### The two K points at pi/3 and 2pi/3
+            bz.HighSymPoints["K2"]      =   @. (2/3) * bz.basis[1]  #####
+    
+        elseif dims == 2    ##### 2d BZ
+    
+            bz.HighSymPoints["M1"]      =  @. 0.5 * bz.basis[1] + 0.0 * bz.basis[2]     ##### The 3 possible M points
+            bz.HighSymPoints["M2"]      =  @. 0.0 * bz.basis[1] + 0.5 * bz.basis[2]
+            bz.HighSymPoints["M3"]      =  @. 0.5 * bz.basis[1] + 0.5 * bz.basis[2]
+    
+            if isapprox(VecAngle(bz.basis[1], bz.basis[2]), 2*pi/3, atol=1e-4, rtol=1e-4)   ##### The K points depend on the relative angle of the reciprocal basis.
+                bz.HighSymPoints["K1"]      =   @. (2/3) * bz.basis[1] + (1/3) * bz.basis[2]
+                bz.HighSymPoints["K2"]      =   @. (1/3) * bz.basis[1] + (2/3) * bz.basis[2]
+    
+            elseif isapprox(VecAngle(bz.basis[1], bz.basis[2]), pi/3, atol=1e-4, rtol=1e-4)
+                bz.HighSymPoints["K1"]      =   @. (1/3) * bz.basis[1] + (1/3) * bz.basis[2]
+                bz.HighSymPoints["K2"]      =   @. (2/3) * bz.basis[1] + (2/3) * bz.basis[2]
+            end
+    
+        end
+    
+    end
 
     @doc """
     ```julia
-    fillBZ!(bz::BZ, uc::UnitCell, offsetRange::Int64=1 ; shift::Vector{Float64}=zeros(Float64, length(uc.primitives)))
+    fillBZ!(bz::BZ, uc::UnitCell ; shift::Vector{Float64}=zeros(Float64, length(uc.primitives)))
     ```
-    Fills the `BZ` object with the relevant attributes, after it has been initialized as `BZ(gridsize=N)`.
+    Fills the `BZ` object with the relevant attributes, after it has been initialized with `BZ(gridSize)`.
 
     """
     function fillBZ!(bz::BZ, uc::UnitCell; shift::Vector{Int64}=zeros(Int64, length(uc.primitives)))
 
-        bz.basis    =   getRLVs(uc)
+        bz.basis    =   getRLVs(uc)   ##### Get the reciprocal basis
         bz.shift    =   shift
         dims 		=	length(uc.primitives)
 
-        @assert length(bz.basis)==2 "Sorry, the code is only written for d=2 right now!"
+        @assert length(bz.gridSize) == dims "Inconsistent dimensions of UnitCell and BZ"
+        @assert length(bz.basis)<=3 "Sorry, the code is only written for upto 3 dimensions right now!"
 
-        inds    =   meshgrid(repeat([bz.gridSize], dims))
+        inds    =   meshgrid(bz.gridSize)
 
+        ##### getting the meshgrid of Monkhorst coefficients of the BZ grid
         for dim in 1:dims
             index   =   getindex.(inds, dim)
-            push!(bz.kInds, Monkhorst.(index, Ref(bz.gridSize), Ref(shift[dim]), Ref(angle(uc.BC[dim]))))
+            push!(bz.kInds, Monkhorst.(index, Ref(bz.gridSize[dim]), Ref(shift[dim]), Ref(angle(uc.BC[dim]))))
         end
 
         bz.ks   =   (bz.kInds[1] .* Ref(bz.basis[1])) 
@@ -131,23 +171,7 @@ module BZone
             bz.ks   .+=     (bz.kInds[dim] .* Ref(bz.basis[dim]))
         end 
 
-        bz.HighSymPoints["Gamma"]   =   zeros(Float64, dims)
-
-        if dims==2
-
-            bz.HighSymPoints["M1"]      =  @. 0.5 * bz.basis[1] + 0.0 * bz.basis[2]
-            bz.HighSymPoints["M2"]      =  @. 0.0 * bz.basis[1] + 0.5 * bz.basis[2]
-            bz.HighSymPoints["M3"]      =  @. 0.5 * bz.basis[1] + 0.5 * bz.basis[2]
-            bz.HighSymPoints["M4"]      =  @. -0.5 * bz.basis[1] + 0.0 * bz.basis[2] ###Added this to test
-
-            if isapprox(VecAngle(bz.basis[1], bz.basis[2]), 2*pi/3, atol=1e-4, rtol=1e-4)
-                bz.HighSymPoints["K1"]      =   @. (2/3) * bz.basis[1] + (1/3) * bz.basis[2]
-                bz.HighSymPoints["K2"]      =   @. (1/3) * bz.basis[1] + (2/3) * bz.basis[2]
-            elseif isapprox(VecAngle(bz.basis[1], bz.basis[2]), pi/3, atol=1e-4, rtol=1e-4)
-                bz.HighSymPoints["K1"]      =   @. (1/3) * bz.basis[1] + (1/3) * bz.basis[2]
-                bz.HighSymPoints["K2"]      =   @. (2/3) * bz.basis[1] + (2/3) * bz.basis[2]
-            end
-        end
+        AddHighSymPoints!(bz)
 
     end
 
@@ -196,24 +220,51 @@ module BZone
     ```
     Returns a path in index-space of the discretized `BZ` which joins the two sets of indices `start` and `ending`. 
     If the input `exclusive` is set to `true`, the returned path will NOT contain the `ending` point itself.
+    Works in any dimensions.
 
     """
     function GetIndexPath(start::Vector{Int64}, ending::Vector{Int64} ; exclusive::Bool=true) :: Vector{Vector{Int64}}
-        @assert length(start)==length(ending)==2 "The function can only return a path in 2-d"
-        diff    =   ending - start
-
-        if diff[1]!=0   ##### Slope is not zero
-            xs  =   collect(start[1] : sign(ending[1] - start[1]) : ending[1] - sign(ending[1] - start[1]) * exclusive)
-            ys  =   round.(Int64 , start[2] .+ (diff[2] / diff[1]) .* (xs .- start[1]))
-        else
-            ys  =   collect(start[2] : sign(ending[2] - start[2]) : ending[2] - sign(ending[2] - start[2]) * exclusive)
-            xs  =   repeat([start[1]], length(ys))
+    
+        dx = ending - start
+        i = findfirst(!=(0), dx)    ##### find the first dimension where the two given points differ.
+        ts = (1/abs(dx[i])) .* collect(0:abs(dx[i]) - exclusive)    ##### choosing the paramtrization of a straight line in generalized dimensiosn s.t. the increment is ±1 along this dimension.
+    
+        coords = []
+        for j in 1:length(start)
+            xj = start[j] .+ (dx[j] .* ts)  ##### a straight line in generalized dimensions b/w two given points : (x[j] - start[j]) / (ending[j] - start[j]) = t ∈ [0, 1] 
+            push!(coords, round.(Int64, xj))
         end
-
-        path    =   hcat(xs, ys)
-        path    =   Vector{eltype(path)}[eachrow(path)...]
-
+    
+        coords = reduce(hcat, coords)   ##### combining all sets of indices into sets of points
+        path = Vector{eltype(coords)}[eachrow(coords)...]
+    
         return path
+    
+    end
+
+
+    @doc """
+    ```julia
+    CombinedIndexPath(bz::BZ, points::Vector{Vector{Float64}} ; nearest::Bool = false, closed::Bool = true) --> Vector{Vector{Int64}}
+    ```
+    Returns a path in index-space joins the given momentum points present in `points` as point[1] --> point[2] --> ... --> point[end] --> point[1].
+    The optional input `nearest` is the same as in [`GetQIndex`](@ref), and `closed` determines if the path is a closed loop or not.
+
+    """
+    function CombinedIndexPath(bz::BZ, points::Vector{Vector{Float64}} ; nearest::Bool = false, closed::Bool = true) :: Vector{Vector{Int64}}
+
+        if closed
+            starts  =   copy(points)
+            endings =   circshift(starts, -1)
+        else
+            starts  =   copy(points[1:end-1])
+            endings =   copy(points[2:end])
+        end
+    
+        paths   =   GetIndexPath.(GetQIndex.(starts, Ref(bz) ; nearest = nearest), GetQIndex.(endings, Ref(bz), nearest=nearest) ; exclusive = true)
+        paths   =   reduce(vcat, paths)
+    
+        return paths
     end
 
 
@@ -231,7 +282,7 @@ module BZone
         end_ind     =   GetQIndex(ending, bz ; nearest=nearest)
         path_ind    =   GetIndexPath(start_ind, end_ind ; exclusive = exclusive)
 
-        return getindex.(Ref(bz.ks), first.(path_ind), last.(path_ind))
+        return getindex.(Ref(bz.ks), CartesianIndex.(Tuple.(path_ind)))
     end
 
 
@@ -240,7 +291,7 @@ module BZone
     CombinedBZPath(bz::BZ, points::Vector{Vector{Float64}} ; nearest::Bool = false, closed::Bool = true) --> Vector{Vector{Float64}}
     ```
     Returns a path in momentum-space of the discretized `BZ` which joins the given momentum points present in `points` as point[1] --> point[2] --> ... --> point[end] --> point[1].
-    The optional input `nearest` is the same as in [`GetQIndex`](@ref), and `closed` determines if the path is a clsoed loop or not.
+    The optional input `nearest` is the same as in [`GetQIndex`](@ref), and `closed` determines if the path is a closed loop or not.
 
     """
     function CombinedBZPath(bz::BZ, points::Vector{Vector{Float64}} ; nearest::Bool = false, closed::Bool = true) :: Vector{Vector{Float64}}
