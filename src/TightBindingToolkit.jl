3709faa7667ac68e77590cbc0b129bb449f61f9c
Anjishnu <anjishnubose98@gmail.com>
2023-07-10 17:10:29 +0530

    Updated package module

--- src/TightBindingToolkit.jl
+++ src/TightBindingToolkit.jl
@@ -1,44 +1,67 @@
 module TightBindingToolkit
 
-# Write your package code here.
+####### Write your package code here.
+
+##### Module to get SU(2) representation for any half-integer / integer spin.
 include("SpinMats.jl")
 using .SpinMatrices
 export SpinMats
 
+include("Useful.jl")
+using .Useful
+export GetAllOffsets, VecAngle, Meshgrid, BinarySearch, DistFunction, DeriDistFunction , GetIndexPath, FFTArrayofMatrix
+
+##### Module for defining Unit Cell structure and some funcitons to manipulate it.
 include("UnitCell.jl")
 using .UCell
-export Bond , isSameBond , UnitCell , isSameUnitCell , getDistance , addBasisSite! , addAnisotropicBond! , addIsotropicBonds! , ModifyBonds! , ScaleBonds! , RemoveBonds! , ModifyFields!, ModifyIsotropicFields!, Lookup
+export Bond , BondRank, IsSameBond , UnitCell , IsSameUnitCell , AddBasisSite! , GetDistance , GetRealSpacePositions, IsSameUnitCell
 
+include("DesignUnitCell.jl")
+using .DesignUCell
+export AddAnisotropicBond! , AddIsotropicBonds! , ModifyBonds! , ScaleBonds! , RemoveBonds! , ModifyFields!, ModifyIsotropicFields!, Lookup
+
+include("ExpandUnitCell.jl")
+using .ExpandUCell
+export ChangePrimitives, ExpandUnitCell, ExpandBonds!
+
+##### Module to define Param structure which represents a general hopping parameter, and can be used to construct a UnitCell.
 include("Params.jl")
 using .Parameters
-export Param, addAnisotropicBond!, addIsotropicBonds!, CreateUnitCell!, ModifyUnitCell!, GetParams
+export Param, AddAnisotropicBond!, AddIsotropicBonds!, CreateUnitCell!, ModifyUnitCell!, GetParams
 
+##### Module to define Brillouin Zone structure and some functions to manipulate it.
 include("BZ.jl")
 using .BZone
-export getRLVs , BZ , fillBZ! , Monkhorst, ReduceQ , GetQIndex , GetIndexPath , CombinedIndexPath , getBZPath , CombinedBZPath, meshgrid
+export GetRLVs , BZ , Monkhorst , FillBZ! , ReduceQ , GetQIndex , CombinedIndexPath , GetBZPath , CombinedBZPath , MomentumPhaseFFT
 
+##### module to define a Hamiltonian structure and solving for some of its properties.
 include("Hamiltonian.jl")
 using .Hams
-export Hamiltonian , FillHoppingHamiltonian, FillPairingHamiltonian, FillHamiltonian , DiagonalizeHamiltonian! , DOS, ModifyHamiltonianField!, isBandGapped
+export Hamiltonian , FillHoppingHamiltonian, FillPairingHamiltonian, FillHamiltonian , DiagonalizeHamiltonian! , DOS, ModifyHamiltonianField!, IsBandGapped
 
+##### Module to define a Tight-Binding Model structure which takes into account thermodynamical parameters such as temperature and filling etc.
 include("Model.jl")
 using .TBModel
-export Model , dist , distDer , findFilling , getMu! , getFilling! , getCount , getGk! , SolveModel! , BinarySearch
+export Model , FindFilling , GetMu! , GetFilling! , GetCount , GetGk! , GetGr!, SolveModel!
 
+##### Module to define the equivalent but for bdG systems with pairing.
 include("BdGModel.jl")
 using .BdG
-export BdGModel, findFilling, getMu!, getFilling!, getGk!, SolveModel!
+export BdGModel, FindFilling , GetMu! , GetFilling! , GetGk! , GetGr!, SolveModel!
 
+##### Module to calculate generalized Chern numbers
 include("Chern.jl")
 using .Chern
 export FindLinks , FieldStrength , ChernNumber, CheckValidity
 
+##### Module to calculate generalized magnetic susceptibility
 include("Susceptibility.jl")
 using .suscep
-export susceptibility , FindChi , FillChis!
+export Susceptibility , FindChi , FillChis!
 
+##### Module containing some plotting functions
 include("Plot.jl")
 using .PlotTB
-export plot_UnitCell! , plot_band_contour!, plot_band_structure!, plot_FS!
+export Plot_UnitCell! , Plot_Band_Contour!, Plot_Band_Structure!, Plot_FS!
 
 end
