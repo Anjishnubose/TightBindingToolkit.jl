ac3dcfafe453e512a95cbbd0f1922518ae35c883
Anjishnu Bose <anjishnubose98@gmail.com>
2023-09-20 10:38:13 -0400

    vortex in real space

--- Sample/Vortex.jl
+++ Sample/Vortex.jl
@@ -7,11 +7,11 @@ a1   =  [ 3/2, sqrt(3)/2]
 a2   =  [-3/2, sqrt(3)/2]
 
 b1   =  [ 0.0 , 0.0 ]
-b2   =  [ 0.0 , 1.0/sqrt(3) ]
-b3   =  [-1.0 , 1.0/sqrt(3) ]
-b4   =  [-1/2 , sqrt(3)/2   ]
-b5   =  [ 1.0 , 1.0/sqrt(3) ]
-b6   =  [ 1/2 , sqrt(3)/2   ]
+b2   =  [ 1/2 , -1/(2 * sqrt(3)) ]
+b3   =  [ 1/2 , -sqrt(3)/(2) ]
+b4   =  [ 0.0 , -2/sqrt(3)   ]
+b5   =  [-1/2 , -sqrt(3)/(2) ]
+b6   =  [-1/2 , -1/(2 * sqrt(3))]
 
 firstNNdistance     = 1.0/sqrt(3)
 secondNNdistance    = 1.0
@@ -24,36 +24,58 @@ AddBasisSite!.(Ref(UC), [b1, b2, b3, b4, b5, b6])
 SpinVec     =   SpinMats(1//2)
 
 const tIntra  =     1.0
-const tInter  =     -1.0
+const tInter  =     -2.5
 
 tIntraParam     =   Param(tIntra, 2)
-AddAnisotropicBond!(tIntraParam, UC, 3, 1, [ 0,  1], SpinVec[4], firstNNdistance, "Intra hopping")
-AddAnisotropicBond!(tIntraParam, UC, 1, 5, [-1,  0], SpinVec[4], firstNNdistance, "Intra hopping")
-AddAnisotropicBond!(tIntraParam, UC, 4, 2, [ 0,  0], SpinVec[4], firstNNdistance, "Intra hopping")
-AddAnisotropicBond!(tIntraParam, UC, 2, 6, [ 0,  0], SpinVec[4], firstNNdistance, "Intra hopping")
-AddAnisotropicBond!(tIntraParam, UC, 5, 4, [ 0, -1], SpinVec[4], firstNNdistance, "Intra hopping")
-AddAnisotropicBond!(tIntraParam, UC, 6, 3, [ 1,  0], SpinVec[4], firstNNdistance, "Intra hopping")
+AddAnisotropicBond!(tIntraParam, UC, 1, 2, [ 0, 0], SpinVec[4], firstNNdistance, "Intra hopping")
+AddAnisotropicBond!(tIntraParam, UC, 2, 3, [ 0, 0], SpinVec[4], firstNNdistance, "Intra hopping")
+AddAnisotropicBond!(tIntraParam, UC, 3, 4, [ 0, 0], SpinVec[4], firstNNdistance, "Intra hopping")
+AddAnisotropicBond!(tIntraParam, UC, 4, 5, [ 0, 0], SpinVec[4], firstNNdistance, "Intra hopping")
+AddAnisotropicBond!(tIntraParam, UC, 5, 6, [ 0, 0], SpinVec[4], firstNNdistance, "Intra hopping")
+AddAnisotropicBond!(tIntraParam, UC, 6, 1, [ 0, 0], SpinVec[4], firstNNdistance, "Intra hopping")
 
 
 tInterParam     =   Param(tInter, 2)
-AddAnisotropicBond!(tInterParam, UC, 2, 1, [ 0,  0], SpinVec[4], firstNNdistance, "Inter hopping")
-AddAnisotropicBond!(tInterParam, UC, 3, 4, [ 0,  0], SpinVec[4], firstNNdistance, "Inter hopping")
-AddAnisotropicBond!(tInterParam, UC, 5, 6, [ 0,  0], SpinVec[4], firstNNdistance, "Inter hopping")
+AddAnisotropicBond!(tInterParam, UC, 1, 4, [ 1,  1], SpinVec[4], firstNNdistance, "Inter hopping")
+AddAnisotropicBond!(tInterParam, UC, 3, 6, [ 0, -1], SpinVec[4], firstNNdistance, "Inter hopping")
+AddAnisotropicBond!(tInterParam, UC, 5, 2, [-1,  0], SpinVec[4], firstNNdistance, "Inter hopping")
 
 CreateUnitCell!(UC, [tIntraParam, tInterParam])
 
-Plot_UnitCell!(UC ; bond_cmp=:viridis, site_size=10.0, plot_conjugate=false, plot_lattice=true)
 
-kSize   =   6*50 + 3
-bz      =   BZ([kSize, kSize])
-FillBZ!(bz, UC)
+###################### k-Space #################################
 
-H       =   Hamiltonian(UC, bz)
-DiagonalizeHamiltonian!(H)
+# Plot_UnitCell!(UC ; bond_cmp=:viridis, site_size=10.0, plot_conjugate=false, plot_lattice=true)
 
-VortexModel     =   Model(UC, bz, H ; T=0.001, mu=0.0)
-SolveModel!(VortexModel ; get_gap = true)
+# kSize   =   6*50 + 3
+# bz      =   BZ([kSize, kSize])
+# FillBZ!(bz, UC)
+
+# H       =   Hamiltonian(UC, bz)
+# DiagonalizeHamiltonian!(H)
+
+# VortexModel     =   Model(UC, bz, H ; T=0.001, mu=0.0)
+# SolveModel!(VortexModel ; get_gap = true)
 
 # Plot_FS!(H, bz, collect(0.05:0.05:0.5), [7]; cbar=true)
+# Plot_Band_Structure!(VortexModel , [bz.HighSymPoints["G"], bz.HighSymPoints["K1"], bz.HighSymPoints["M2"]] ; labels = [L"\Gamma", L"K_1", L"M_2"] )
 
-Plot_Band_Structure!(VortexModel , [bz.HighSymPoints["G"], bz.HighSymPoints["K1"], bz.HighSymPoints["M2"]] ; labels = [L"\Gamma", L"K_1", L"M_2"] )
+###################### Real Space ######################################
+
+lattice     =   Lattice(UC, [6, 6])
+FillLattice!(lattice)
+
+H = LatticeHamiltonian(lattice)
+DiagonalizeHamiltonian!(H)
+
+plot(H.bands, marker = :circle, label = "Energies")
+
+M   =   LatticeModel(lattice, H)
+
+Ta1 = Translations(lattice ; primitive  = 1, with_local = true)
+Ta2 = Translations(lattice ; primitive  = 2, with_local = true)
+
+m1 = GetPhase.(FindQuantumNumbers(M, Ta1 ; till_band = length(H.bands)÷2))
+m2 = GetPhase.(FindQuantumNumbers(M, Ta2 ; till_band = length(H.bands)÷2))
+
+mGround = mod.([sum(sum.(m1)), sum(sum.(m2))], Ref(1.0))
