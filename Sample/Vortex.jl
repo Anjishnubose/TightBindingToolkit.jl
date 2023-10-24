ffd1db64f5b2b5b93b0c49324ebc4fb983caf844
Anjishnu <anjishnubose98@gmail.com>
2023-10-24 12:07:48 -0400

    updated sample

--- Sample/Vortex.jl
+++ Sample/Vortex.jl
@@ -18,13 +18,14 @@ secondNNdistance    = 1.0
 thirdNNdistance     = 2/sqrt(3)
 
 UC      =   UnitCell([a1, a2], 2, 2)
+UC.BC   =   zeros(ComplexF64, 2)
 
 AddBasisSite!.(Ref(UC), [b1, b2, b3, b4, b5, b6])
 
 SpinVec     =   SpinMats(1//2)
 
 const tIntra  =     1.0
-const tInter  =     -2.5
+const tInter  =     -10.0
 
 tIntraParam     =   Param(tIntra, 2)
 AddAnisotropicBond!(tIntraParam, UC, 1, 2, [ 0, 0], SpinVec[4], firstNNdistance, "Intra hopping")
@@ -45,7 +46,7 @@ CreateUnitCell!(UC, [tIntraParam, tInterParam])
 
 ###################### k-Space #################################
 
-# Plot_UnitCell!(UC ; bond_cmp=:viridis, site_size=10.0, plot_conjugate=false, plot_lattice=true)
+UCPlot = Plot_UnitCell!(UC ; bond_cmp=:viridis, site_size=10.0, plot_conjugate=false, plot_lattice=true)
 
 # kSize   =   6*50 + 3
 # bz      =   BZ([kSize, kSize])
@@ -69,13 +70,16 @@ H = LatticeHamiltonian(lattice)
 DiagonalizeHamiltonian!(H)
 
 plot(H.bands, marker = :circle, label = "Energies")
+Plot_Lattice!(lattice ; bond_cmp=:viridis, site_size=10.0)
 
-M   =   LatticeModel(lattice, H)
+# M   =   LatticeModel(lattice, H)
 
-Ta1 = Translations(lattice ; primitive  = 1, with_local = true)
-Ta2 = Translations(lattice ; primitive  = 2, with_local = true)
+######################## Symmetry ##########################
 
-m1 = GetPhase.(FindQuantumNumbers(M, Ta1 ; till_band = length(H.bands)÷2))
-m2 = GetPhase.(FindQuantumNumbers(M, Ta2 ; till_band = length(H.bands)÷2))
+# Ta1 = Translations(lattice ; primitive  = 1, with_local = true)
+# Ta2 = Translations(lattice ; primitive  = 2, with_local = true)
 
-mGround = mod.([sum(sum.(m1)), sum(sum.(m2))], Ref(1.0))
+# m1 = GetPhase.(FindQuantumNumbers(M, Ta1 ; till_band = length(H.bands)÷2))
+# m2 = GetPhase.(FindQuantumNumbers(M, Ta2 ; till_band = length(H.bands)÷2))
+
+# mGround = mod.([sum(sum.(m1)), sum(sum.(m2))], Ref(1.0))
