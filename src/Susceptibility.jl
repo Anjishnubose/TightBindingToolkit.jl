8d71eb8fb77b5d594b1938b8e70372bc801e75dc
Anjishnu Bose <anjishnubose98@gmail.com>
2023-10-25 18:21:09 -0400

    Fixed some errors. RPA still not working correctly

--- src/Susceptibility.jl
+++ src/Susceptibility.jl
@@ -1,5 +1,5 @@
 module suscep
-    export Susceptibility , FindChi , FillChis!
+    export Susceptibility , FindChi , FillChis!, FillRPAChis!, PerformRPA!, FindReducedChi, nF_factor, ResolvedQInteraction
 
     using ..TightBindingToolkit.SpinMatrices:SpinMats
     using ..TightBindingToolkit.Useful: DistFunction, SwitchKroneckerBasis, ImpIndices
@@ -16,14 +16,14 @@ module suscep
 
     function ResolvedQInteraction(Q::Vector{Float64}, interaction::Param{T, Float64}, uc::UnitCell{T2})::Matrix{Array{ComplexF64, T}} where {T, T2}
 
-        resolvedInt     =   repeat([zeros(ComplexF64, size(interaction.unitBond[begin].bondMat))], length(uc.basis), length(uc.basis))     ##### Initializing the sublattice resolved interaction matrix
+        resolvedInt     =   repeat([zeros(ComplexF64, size(interaction.unitBonds[begin].mat))], length(uc.basis), length(uc.basis))     ##### Initializing the sublattice resolved interaction matrix
         flippedIndices 	=	collect(T:-1:1)
 
 
         for bond in interaction.unitBonds
 
-            contribution    =   bond.mat * exp.(im .* dot.(Ref(Q) , bond.offset .* uc.primitives))     ##### Adding the bond contribution to the resolved interaction matrix
-            contribution    +=  collect(conj.(permutedims(bond.mat, flippedIndices))) * exp.(-im .* dot.(Ref(Q) , bond.offset .* uc.primitives))    ##### Adding the hermitian conjugate bond contribution to the resolved interaction matrix
+            contribution    =   interaction.value[end] * bond.mat * exp.(im .* dot(Q, sum(bond.offset .* uc.primitives)))     ##### Adding the bond contribution to the resolved interaction matrix
+            contribution    +=  interaction.value[end] * collect(conj.(permutedims(bond.mat, flippedIndices))) * exp.(-im .* dot(Q, sum(bond.offset .* uc.primitives)))    ##### Adding the hermitian conjugate bond contribution to the resolved interaction matrix
 
             resolvedInt[bond.base, bond.target]     +=  contribution / 2
             resolvedInt[bond.target, bond.base]     +=  contribution / 2
@@ -103,12 +103,12 @@ function to calculate ``(nF(E(k)) - nF(E(k+Q))) / (Ω + i * η - (E(k+Q) - E(k))
         
         Es_k    =   M.Ham.bands
         Es_kpQ  =   circshift(Es_k, -Q_index)  ##### The bands with momentum indices rolled to k+Q instead of k.
-        dEs     =   reshape(map((x, y) -> x .- y, Es_kpQ, Es_k), length(M.Ham.bands))  ##### The energy differences between the bands at k+Q and k. Flattened momentum index.
+        dEs     =   reshape(map((x, y) -> x .- y', Es_kpQ, Es_k), length(M.Ham.bands))  ##### The energy differences between the bands at k+Q and k. Flattened momentum index.
 
         ##### The fermi distribution functions for each band at each k-point
         nf_k    =   DistFunction.(Es_k ; T=M.T, mu=M.mu, stat=M.stat) 
         nf_kpQ  =   circshift(nf_k, -Q_index)
-        dnFs    =   reshape(map((x, y) -> x .- y, nf_k, nf_kpQ), length(M.Ham.bands))  ##### The fermi distribution function differences between the bands at k+Q and k. Flatened momentum index.
+        dnFs    =   reshape(map((x, y) -> x .- y', nf_k, nf_kpQ), length(M.Ham.bands))  ##### The fermi distribution function differences between the bands at k+Q and k. Flatened momentum index.
 
         @cast nF[k][i, j] |= dnFs[k][i, j] / (Omega + im * eta - dEs[k][j, i]);
         return nF
@@ -160,11 +160,11 @@ function to calculate susceptibility at a fixed Ω=`Omega`, and `Q`, and along a
         for (i, base) in enumerate(M.uc.basis)
             for (j, target) in enumerate(M.uc.basis)
 
-                Uis     =   selectdim.(Uk, 1, (i - 1) * M.uc.localDim + 1 : i * M.uc.localDim)
-                Ujs     =   selectdim.(adjoint.(Uk), 2, (j - 1) * M.uc.localDim + 1 : j * M.uc.localDim)
+                Uis     =   selectdim.(Uk, 1, Ref((i - 1) * M.uc.localDim + 1 : i * M.uc.localDim))
+                Ujs     =   selectdim.(adjoint.(Uk), 2, Ref((j - 1) * M.uc.localDim + 1 : j * M.uc.localDim))
 
-                UisQ    =   selectdim.(adjoint.(UkQ), 2, (i - 1) * M.uc.localDim + 1 : i * M.uc.localDim)
-                UjsQ    =   selectdim.(UkQ, 1, (j - 1) * M.uc.localDim + 1 : j * M.uc.localDim)
+                UisQ    =   selectdim.(adjoint.(UkQ), 2, Ref((i - 1) * M.uc.localDim + 1 : i * M.uc.localDim))
+                UjsQ    =   selectdim.(UkQ, 1, Ref((j - 1) * M.uc.localDim + 1 : j * M.uc.localDim))
 
                 Ma      =   reshape(UisQ .* Ref(M.uc.OnSiteMats[a]) .* Uis, length(M.Ham.bands))
                 Mb      =   reshape(Ujs .* Ref(M.uc.OnSiteMats[b]) .* UjsQ, length(M.Ham.bands))
@@ -180,12 +180,12 @@ function to calculate susceptibility at a fixed Ω=`Omega`, and `Q`, and along a
 
     function PerformRPA!(chi::Susceptibility, GeneratorInteraction::Param{2, Float64}, uc::UnitCell{T}) where {T}
         ##### GeneratorInteraction refers to the interaction written in the basis of all the generators of the local Hilbert space (including the identity matrix). Eg. Id + SU(2) spins -> GeneratorInteraction is a 4x4 matrix.
-        resolvedInts    =   ResolvedQInteraction.(chi.Qs, Ref(interaction), Ref(chi.M.uc))
-        resolvedInts    =   map(x -> hvcat(length(uc.basis), permutedims(resolvedInts, [2, 1])...), resolvedInts)  ##### Reshaping the resolved interactions to be a vector of matrices, with the first index being the momentum index, and the second being the sublattice x Generator index.
+        resolvedInts    =   ResolvedQInteraction.(chi.Qs, Ref(GeneratorInteraction), Ref(uc))
+        resolvedInts    =   map(x -> hvcat(length(uc.basis), permutedims(x, [2, 1])...), resolvedInts)  ##### Reshaping the resolved interactions to be a vector of matrices, with the first index being the momentum index, and the second being the sublattice x Generator index.
         permutation     =   SwitchKroneckerBasis((length(uc.OnSiteMats), length(uc.basis)))
         map!(x -> x[permutation, permutation], resolvedInts, resolvedInts)  ##### Switching the basis of the resolved interactions to be a vector of matrices into the generator x sublattice index
         resolvedInts    =   permutedims(reshape(repeat(resolvedInts, length(chi.Omegas)), (length(chi.Qs), length(chi.Omegas))), [2, 1])    ##### Repeating the resolved interactions to be a matrix of matrices, with the outermatrix having the (energy, momentum) index, and the inner being sublattice x Generator index.
-        corrections     =   inv(Ref(I) .- resolvedInts .* chi.Bare)  ##### The RPA corrections to the bare susceptibility coming from resummation
+        corrections     =   inv.(Ref(I) .- (resolvedInts .* chi.Bare))  ##### The RPA corrections to the bare susceptibility coming from resummation
         chi.RPA         =   chi.Bare .* corrections  ##### The RPA susceptibility
     end
 
@@ -200,28 +200,40 @@ function to calculate susceptibility at a all given Ω=`Omegas`, but for all `Q`
         Gamma_index = GetQIndex(zeros(Float64, length(M.uc.primitives)), M.bz ; nearest = false)    ##### The index of the Gamma point in the BZ grid.
         @assert !isempty(Gamma_index) "The Gamma point needs to be in the BZ grid for this calculation to work!" 
 
-        if fill_BZ
-            chi.Qs  =   reshape(M.bz.ks, length(M.bz.ks))
-        end
-
         if reduce   ##### Calculate only the reduced susceptibility
-            chis    =   FindReducedChi.(chi.Omegas, adjoint(chi.Qs), Ref(chi.M), ; a=a, b=b, eta=chi.Spread)
+            chis    =   FindReducedChi.(chi.Omegas, reshape(chi.Qs, 1, length(chi.Qs)), Ref(M), ; a=a, b=b, eta=chi.Spread)
             chis    =   chis ./ length(M.bz.ks)
 
-            setindex!.(chi.BareReduced, chis, Ref(CartesianIndex(a, b)))
+            chisTot     =   [zeros(ComplexF64, length(M.uc.OnSiteMats), length(M.uc.OnSiteMats)) for i in 1:length(chi.Omegas), j in 1:length(chi.Qs)]
+            setindex!.(chisTot, chis, Ref(CartesianIndex(a, b)))
+            chi.BareReduced     =   chisTot
+
         else    ##### Calculate the sublattice resolved susceptibility
-            chis    =   FindChi.(chi.Omegas, adjoint(chi.Qs), Ref(chi.M), ; a=a, b=b, eta=chi.Spread)
+            chis    =   FindChi.(chi.Omegas, reshape(chi.Qs, 1, length(chi.Qs)), Ref(M), ; a=a, b=b, eta=chi.Spread)
             chis    =   chis ./ length(M.bz.ks)
             ##### Setting the appropriate indices of the susceptibility matrix which is generator x sublattice.
             for (i, base) in enumerate(M.uc.basis)
                 for (j, target) in enumerate(M.uc.basis)
 
-                    setindex!.(chi.Bare, Ref(getindex.(chis, i, j)), Ref(CartesianIndex(length(M.uc.basis) * (a - 1) + i, length(M.uc.basis) * (b - 1) + j)))
+                    setindex!.(chi.Bare, getindex.(chis, i, j), Ref(CartesianIndex(length(M.uc.basis) * (a - 1) + i, length(M.uc.basis) * (b - 1) + j)))
                 end
             end
+
         end
         
         @info "Chis filled along $(a)-$(b)."
+    end
+
+
+    function FillRPAChis!(chi::Susceptibility, M::Model, GeneratorInteraction::Param{2, Float64} ; Generators::Vector{Vector{Int64}} = [[i, i] for i in 1:length(M.uc.OnSiteMats)])
+
+        for generator in Generators
+            FillChis!(chi, M, ; a=generator[1], b=generator[2], reduce=false)
+        end
+
+        PerformRPA!(chi, GeneratorInteraction, M.uc)
+
+        @info "RPA performed!."
     end
 
 
